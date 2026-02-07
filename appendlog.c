804f13fe3e49fa531419230e3866e1467f258446
Nick Nyburg <nick@Mac.lan>
2026-02-06 16:56:55 -0800

    Implement fcntl locks and append a record

--- appendlog.c
+++ appendlog.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
@@ -15,6 +16,9 @@
 #define HEADER_SIZE ((int)(sizeof(int) * HEADER_INTS))
 #define FILE_SIZE ((off_t)HEADER_SIZE + (off_t)MAX_RECORDS * (off_t)RECORD_SIZE)
 
+#define HEADER_OFFSET_INDEX 0
+#define HEADER_COUNT_INDEX  1
+
 static void *map_log_file(int fd) {
     void *addr = mmap(NULL, (size_t)FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (addr == MAP_FAILED) {
@@ -24,6 +28,20 @@ static void *map_log_file(int fd) {
     return addr;
 }
 
+static void lock_region(int fd, short lock_type, off_t start, off_t len) {
+    struct flock fl;
+    memset(&fl, 0, sizeof(fl));
+    fl.l_type = lock_type;
+    fl.l_whence = SEEK_SET;
+    fl.l_start = start;
+    fl.l_len = len;
+
+    if (fcntl(fd, F_SETLKW, &fl) != 0) {
+        perror("fcntl(F_SETLKW)");
+        exit(1);
+    }
+}
+
 static int parse_int(const char *s) {
     char *end = NULL;
     long v = strtol(s, &end, 10);
@@ -31,6 +49,27 @@ static int parse_int(const char *s) {
         return -1;
     }
     return (int)v;
+}
+
+static void append_one_record(int fd, void *map, const char *text32) {
+    int *header = (int *)map;
+
+    lock_region(fd, F_WRLCK, (off_t)0, (off_t)HEADER_SIZE);
+
+    int old_offset = header[HEADER_OFFSET_INDEX];
+    int old_count  = header[HEADER_COUNT_INDEX];
+
+    header[HEADER_COUNT_INDEX] = old_count + 1;
+    header[HEADER_OFFSET_INDEX] = old_offset + RECORD_SIZE;
+
+    lock_region(fd, F_UNLCK, (off_t)0, (off_t)HEADER_SIZE);
+
+    off_t rec_start = (off_t)HEADER_SIZE + (off_t)old_offset;
+    char *rec_ptr = (char *)map + rec_start;
+
+    lock_region(fd, F_WRLCK, rec_start, (off_t)RECORD_SIZE);
+    memcpy(rec_ptr, text32, (size_t)RECORD_SIZE);
+    lock_region(fd, F_UNLCK, rec_start, (off_t)RECORD_SIZE);
 }
 
 int main(int argc, char **argv) {
@@ -47,7 +86,6 @@ int main(int argc, char **argv) {
     }
 
     void *map = map_log_file(fd);
-    (void)map;
 
     int argn = argc - 1;
     if (argn < 2 || (argn % 2) != 0) {
@@ -85,8 +123,14 @@ int main(int argc, char **argv) {
         }
 
         if (pid == 0) {
-            (void)count;
-            (void)prefix;
+            for (int n = 0; n < count; n++) {
+                char rec[RECORD_SIZE];
+                memset(rec, 0, sizeof(rec));
+                (void)snprintf(rec, sizeof(rec), "%s %d", prefix, n);
+
+                append_one_record(fd, map, rec);
+            }
+
             munmap(map, (size_t)FILE_SIZE);
             close(fd);
             _exit(0);
